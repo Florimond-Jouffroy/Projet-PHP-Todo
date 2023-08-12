87b2b4599a06e56c2ef09a23f071e62ae9e79375
Florimond Jouffroy <florimond.jouffroy@gmail.com>
2023-08-12 17:38:15 +0200

    comments Router & Route

--- src/services/Route.php
+++ src/services/Route.php
@@ -2,14 +2,13 @@
 
 namespace App\Services;
 
-
 class Route
 {
-  private $method;
-  private $path;
-  private $controller;
-  private $action;
-  private $pattern;
+  private $method;        // Méthode HTTP associée à la route (GET, POST, etc.)
+  private $path;          // Chemin de l'URL associé à la route (par exemple, '/tasks/show/{id}')
+  private $controller;    // Nom du contrôleur à appeler lors de la correspondance de la route
+  private $action;        // Action du contrôleur à appeler lors de la correspondance de la route
+  private $pattern;       // Modèle d'expression régulière généré à partir du chemin
 
   public function __construct($method, $path, $controller, $action)
   {
@@ -17,32 +16,36 @@ class Route
     $this->path = $path;
     $this->controller = $controller;
     $this->action = $action;
-    $this->pattern = $this->getPattern();
+    $this->pattern = $this->getPattern(); // Calcul du modèle d'expression régulière au moment de la création de la route
   }
 
+  // Vérifie si la route correspond à la méthode et à l'URI donnés
   public function matches($method, $uri)
   {
     return $this->method === $method && preg_match($this->pattern, $uri);
   }
 
+  // Exécute l'action du contrôleur correspondant avec les paramètres capturés dans l'URI
   public function execute($uri)
   {
     $matches = $this->getMatches($uri);
     $controllerName = 'App\\Controllers\\' . $this->controller;
     $actionName = $this->action;
     $controller = new $controllerName();
-    $controller->$actionName(...$matches);
+    $controller->$actionName(...$matches); // Appel de l'action du contrôleur avec les paramètres capturés
   }
 
+  // Génère un modèle d'expression régulière à partir du chemin de la route avec des paramètres
   private function getPattern()
   {
     return '~^' . preg_replace('/\{([a-zA-Z0-9_]+)\}/', '([a-zA-Z0-9_]+)', $this->path) . '$~';
   }
 
+  // Récupère les paramètres capturés par l'expression régulière dans l'URI
   private function getMatches($uri)
   {
     preg_match($this->pattern, $uri, $matches);
-    array_shift($matches);
-    return $matches;
+    array_shift($matches); // Supprime le premier élément, qui contient la correspondance complète
+    return $matches; // Retourne un tableau des paramètres capturés
   }
 }
--- src/services/Router.php
+++ src/services/Router.php
@@ -6,38 +6,43 @@ use App\Services\Route;
 
 class Router
 {
-  private $routes = array();
+  private $routes = array(); // Tableau pour stocker les objets Route
 
+  // Ajoute une nouvelle route à la liste des routes
   public function addRoute($method, $path, $controller, $action)
   {
-    $this->routes[] = new Route($method, $path, $controller, $action);
+    $this->routes[] = new Route($method, $path, $controller, $action); // Crée et ajoute un nouvel objet Route à la liste
   }
 
+  // Gère la demande en fonction de la méthode HTTP et de l'URI
   public function handleRequest($method, $uri)
   {
     foreach ($this->routes as $route) {
-      if ($route->matches($method, $uri)) {
-        $route->execute($uri);
-        return;
+      if ($route->matches($method, $uri)) { // Vérifie si la route correspond à la méthode et à l'URI
+        $route->execute($uri); // Exécute l'action du contrôleur associé à la route
+        return; // Sort de la boucle dès qu'une correspondance est trouvée
       }
     }
 
-    $this->notFound();
+    $this->notFound(); // Appelle la méthode notFound si aucune correspondance n'est trouvée
   }
 
+  // Affiche une erreur 404 et un message approprié
   private function notFound()
   {
-    header("HTTP/1.0 404 Not Found");
-    echo "404 Not Found";
+    header("HTTP/1.0 404 Not Found"); // Envoie l'en-tête HTTP 404 Not Found
+    echo "404 Not Found"; // Affiche un message d'erreur
   }
 
+  // Méthode pour ajouter une route de type GET
   public function get($path, $controller, $action)
   {
-    $this->addRoute('GET', $path, $controller, $action);
+    $this->addRoute('GET', $path, $controller, $action); // Appelle la méthode addRoute avec la méthode GET
   }
 
+  // Méthode pour ajouter une route de type POST
   public function post($path, $controller, $action)
   {
-    $this->addRoute('POST', $path, $controller, $action);
+    $this->addRoute('POST', $path, $controller, $action); // Appelle la méthode addRoute avec la méthode POST
   }
 }
